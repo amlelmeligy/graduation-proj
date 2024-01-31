611f8bd321cf24c74fce89f4ad3929893e7c3c4b
aml_gamal <amlgamal@std.mans.edu.eg>
2024-01-31 17:33:08 +0200

    second

--- proj/lib/home/Home.dart
+++ proj/lib/home/Home.dart
@@ -50,7 +50,7 @@ class _HomeState extends State<Home> {
     return MaterialApp(
         home: Scaffold(
             bottomNavigationBar: BottomNavigationBar(
-              iconSize: 28,
+              iconSize: 30,
               items: const [
                 BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
                 BottomNavigationBarItem(
